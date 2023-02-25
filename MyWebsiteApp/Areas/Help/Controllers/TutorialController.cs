de86617c31e586c5e767b60c1ee4f2d35094ca24
Mike Berg <68578047+Mike-Berg-98605@users.noreply.github.com>
2023-02-25 16:00:57 -0500

    main

---
+++ MyWebsiteApp/Areas/Help/Controllers/TutorialController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebsiteApp.Areas.Admin.Controllers
+{
+    [Area("Help")]
+    public class TutorialController : Controller
+    {        
+        public IActionResult Page1()
+        {
+            return View();
+        }
+       
+        public IActionResult Page2()
+        {
+            return View();
+        }
+
+        public IActionResult Page3()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyWebsiteApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebsiteApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Phone = "555-123-4567";
+            ViewBag.Email = "me@me.com";
+            ViewBag.Facebook = "Don't use facebook";
+
+            return View();
+        }
+    }
+}
